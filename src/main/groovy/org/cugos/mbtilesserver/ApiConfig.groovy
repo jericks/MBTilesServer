54104fe22d0b25c00742533e0f94168af9108813
jericks <jared.erickson@gmail.com>
2019-06-02 10:55:56 -0700

    Add swagger docs

---
+++ src/main/groovy/org/cugos/mbtilesserver/ApiConfig.groovy
@@ -0,0 +1,37 @@
+package org.cugos.mbtilesserver
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+@Configuration
+@EnableSwagger2
+class ApiConfig {
+
+    @Bean
+    Docket api() {
+        new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("org.cugos.mbtilesserver"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("MBTiles REST API")
+                .description("MBTilesServer REST API")
+                .version("1.0.0")
+                .license("MIT")
+                .licenseUrl("https://opensource.org/licenses/MIT")
+                .build();
+    }
+
+}
--- src/main/groovy/org/cugos/mbtilesserver/Rest.groovy
+++ src/main/groovy/org/cugos/mbtilesserver/Rest.groovy
@@ -7,6 +7,9 @@ import geoscript.layer.io.PyramidWriter
 import geoscript.layer.io.JsonPyramidWriter
 import geoscript.proj.Projection
 import groovy.json.JsonOutput
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -23,17 +26,20 @@ import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 import org.springframework.web.servlet.ModelAndView
+import springfox.documentation.annotations.ApiIgnore
 
 import javax.imageio.ImageIO
 import javax.servlet.http.HttpServletResponse
 import java.awt.image.RenderedImage
 
+@Api(value = "MBTiles REST API")
 @RestController
 class Rest {
 
     @Autowired
     Config config
 
+    @ApiIgnore
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = "text/html")
     public ModelAndView home(Model model) {
         model.addAttribute("name", config.mbtiles.name)
@@ -42,9 +48,14 @@ class Rest {
         new ModelAndView("home")
     }
 
-    @RequestMapping("raster/{w}/{h}/{bounds}")
+    @RequestMapping(value = "raster/{w}/{h}/{bounds}", method = RequestMethod.GET)
     @ResponseBody
-    HttpEntity<byte[]> raster(@PathVariable int w, @PathVariable int h, @PathVariable String bounds) throws IOException {
+    @ApiOperation(value="Get a Raster", notes="Stitch together a Raster from a set of tiles")
+    HttpEntity<byte[]> raster(
+            @PathVariable @ApiParam(value = "Width") int w,
+            @PathVariable @ApiParam(value = "Height") int h,
+            @PathVariable @ApiParam(value = "Bounds") String bounds
+    ) throws IOException {
         String type = config.mbtiles.metadata.get("format","png") ?: "png"
         Bounds b = Bounds.fromString(bounds)
         b.proj = "EPSG:4326"
@@ -53,15 +64,16 @@ class Rest {
         createHttpEntity(bytes, type)
     }
 
-    @RequestMapping("raster/{z}/{x}/{y}/{proj}/{w}/{h}")
+    @RequestMapping(value = "raster/{z}/{x}/{y}/{proj}/{w}/{h}", method = RequestMethod.GET)
     @ResponseBody
+    @ApiOperation(value="Get a Raster around a Point", notes="Stitch together a Raster from a set of tiles")
     HttpEntity<byte[]> rasterAroundPoint(
-            @PathVariable long z,
-            @PathVariable double x,
-            @PathVariable double y,
-            @PathVariable String proj,
-            @PathVariable int w,
-            @PathVariable int h
+            @PathVariable @ApiParam(value = "Zoom Level") long z,
+            @PathVariable @ApiParam(value = "X Coordinate") double x,
+            @PathVariable @ApiParam(value = "Y Coordinate") double y,
+            @PathVariable @ApiParam(value = "Projection") String proj,
+            @PathVariable @ApiParam(value = "Width") int w,
+            @PathVariable @ApiParam(value = "Height") int h
     ) throws IOException {
         String type = config.mbtiles.metadata.get("format","png") ?: "png"
         Point point = Projection.transform(new Point(x,y), new Projection(proj), config.mbtiles.proj)
@@ -72,7 +84,12 @@ class Rest {
 
     @RequestMapping(value = "tile/{z}/{x}/{y}", method = RequestMethod.GET)
     @ResponseBody
-    HttpEntity<byte[]> getTile(@PathVariable int z, @PathVariable int x, @PathVariable int y) throws IOException {
+    @ApiOperation(value="Get a Tile", notes="Get a Tile")
+    HttpEntity<byte[]> getTile(
+      @PathVariable @ApiParam(value = "Zoom Level") int z, 
+      @PathVariable @ApiParam(value = "X") int x, 
+      @PathVariable @ApiParam(value = "Y") int y
+    ) throws IOException {
         ImageTile tile = config.mbtiles.get(z,x,y)
         byte[] bytes = tile.data
         createHttpEntity(bytes, config.mbtiles.metadata.get("format","png")  ?: "png")
@@ -80,7 +97,13 @@ class Rest {
 
     @RequestMapping(value = "tile/{z}/{x}/{y}", method = RequestMethod.POST)
     @ResponseBody
-    HttpEntity<byte[]> createTile(@PathVariable int z, @PathVariable int x, @PathVariable int y, @RequestParam MultipartFile file) throws IOException {
+    @ApiOperation(value="Create a Tile", notes="Create a Tile")
+    HttpEntity<byte[]> createTile(
+      @PathVariable @ApiParam(value = "Zoom Level") int z, 
+      @PathVariable @ApiParam(value = "X") int x, 
+      @PathVariable @ApiParam(value = "Y") int y, 
+      @RequestParam @ApiParam(value = "File") MultipartFile file
+    ) throws IOException {
         if (config.readOnly) {
             new ResponseEntity<byte[]>(HttpStatus.METHOD_NOT_ALLOWED)
         } else {
@@ -94,7 +117,12 @@ class Rest {
 
     @RequestMapping(value = "tile/{z}/{x}/{y}", method = RequestMethod.DELETE)
     @ResponseBody
-    HttpEntity<byte[]> deleteTile(@PathVariable int z, @PathVariable int x, @PathVariable int y) throws IOException {
+    @ApiOperation(value="Delete a Tile", notes="Delete a Tile")
+    HttpEntity<byte[]> deleteTile(
+      @PathVariable @ApiParam(value = "Zoom Level") int z, 
+      @PathVariable @ApiParam(value = "X") int x, 
+      @PathVariable @ApiParam(value = "Y") int y
+    ) throws IOException {
         if (config.readOnly) {
             new ResponseEntity<byte[]>(HttpStatus.METHOD_NOT_ALLOWED)
         } else {
@@ -107,6 +135,7 @@ class Rest {
 
     @RequestMapping(value = "tile/counts", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
+    @ApiOperation(value="Count Tiles", notes="Count Tiles")
     String tileCounts() throws IOException {
         JsonOutput.prettyPrint(
             JsonOutput.toJson(config.mbtiles.tileCounts)
@@ -115,6 +144,7 @@ class Rest {
 
     @RequestMapping(value = "pyramid", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
+    @ApiOperation(value="Get Pyramid", notes="Get the tile pyramid")
     String pyramid() throws IOException {
         PyramidWriter writer = new JsonPyramidWriter()
         writer.write(config.mbtiles.pyramid)
@@ -122,6 +152,7 @@ class Rest {
 
     @RequestMapping(value = "metadata", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
+    @ApiOperation(value="Get Metadata", notes="Get the tile metadata")
     String metadata() throws IOException {
         JsonOutput.prettyPrint(
             JsonOutput.toJson(config.mbtiles.metadata)
@@ -130,6 +161,7 @@ class Rest {
 
     @RequestMapping(value = "gdal", method = RequestMethod.GET, produces = "text/xml")
     @ResponseBody
+    @ApiOperation(value="Get GDAL Layer", notes="Get GDAL Layer")
     String gdal() throws IOException {
         """<GDAL_WMS>
     <Service name="TMS">
